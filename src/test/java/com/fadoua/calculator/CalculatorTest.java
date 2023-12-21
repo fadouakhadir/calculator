b804b844ba0859bff5e254d034d8b15cdca11364
fadouakhadir <khadirfadoua397@gmail.com>
2023-12-21 11:50:43 +0100

    jenkins

--- src/test/java/com/fadoua/calculator/CalculatorTest.java
+++ src/test/java/com/fadoua/calculator/CalculatorTest.java
@@ -1,6 +1,6 @@
 package com.fadoua.calculator;
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 private Calculator calculator = new Calculator();
